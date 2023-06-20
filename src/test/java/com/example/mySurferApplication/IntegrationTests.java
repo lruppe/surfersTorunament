94e9396a8fea360aaa0ed3ade54aeb73c04b4980
lruppe <leon_ruppen@hotmail.com>
2023-06-20 10:02:41 +0200

    removed old junit dependency

--- src/test/java/com/example/mySurferApplication/IntegrationTests.java
+++ src/test/java/com/example/mySurferApplication/IntegrationTests.java
@@ -2,18 +2,15 @@ package com.example.mySurferApplication;
 
 import com.example.mySurferApplication.DemoApplication.Services.ContestService;
 import com.example.mySurferApplication.DemoApplication.Services.Entities.Contest;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class IntegrationTests {
 
@@ -22,7 +19,6 @@ public class IntegrationTests {
 
     @Autowired
     private ContestService contestService;
-
     @Test
     public void whenCreateContest_thenReturnContest(){
 
